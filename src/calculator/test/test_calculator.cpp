84c734f228aa08efbd8aa9abc1826b372343162a
drdrag <dragostin.kozhuharov@gmail.com>
2023-06-15 19:54:11 +0300

    added unit test using GTest

---
+++ src/calculator/test/test_calculator.cpp
@@ -0,0 +1,18 @@
+//
+// Created by dkozh on 15-Jun-23.
+//
+#include <gtest.h>
+#include "calculator.h"
+
+TEST(CalculatorTest, testAddition)
+{
+    Calculator mCalculator{5, '+',6};
+    double result = mCalculator.calculate();
+    ASSERT_EQ(11, result);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv)
+    return RUN_ALL_TESTS();
+}
